0afe24f0cc83e1ee07089a18b1b163e838524808
Mitch Sullivan <sullivan.mitch@gmail.com>
2021-10-17 15:34:41 -0230

    Initial commit of the message.Message class

--- src/message.py
+++ src/message.py
@@ -2,7 +2,12 @@
 Message module
 """
 from __future__ import annotations
+from datetime import datetime
 from enum import auto, IntEnum
+from typing import List
+
+
+TIMESTAMP_FORMAT: str = "%Y%m%d%H%M%S%f"
 
 
 class MessageType(IntEnum):
@@ -30,3 +35,52 @@ class MessageType(IntEnum):
         Convert a `MessageType` object to a string.
         """
         return self.name.lower()
+
+
+class Message(object):
+    """
+    Message class
+    """
+
+    @classmethod
+    def from_string(cls: Message, message_string: str) -> Message:
+        """
+        Create a `Message` object from a message string.
+        """
+        message_type_string, timestamp_string, *payload = message_string.split(",")
+        message_type: MessageType = MessageType.from_string(message_type_string)
+        timestamp: datetime = datetime.strptime(timestamp_string, TIMESTAMP_FORMAT)
+        return cls(message_type, timestamp, *payload)
+
+    @classmethod
+    def from_bytes(cls: Message, message_bytes: bytes) -> Message:
+        """
+        Create a `Message` object from a binary string.
+        """
+        message_string: str = message_bytes.decode("utf-8")
+        return cls.from_string(message_string)
+
+    def __init__(self: Message, message_type: MessageType, timestamp: datetime, *payload: str) -> None:
+        """
+        Initialize a `Message` object with a message type, a timestamp, and a payload.
+        """
+        self.message_type: MessageType = message_type
+        self.timestamp: datetime = timestamp
+        self.payload: List[str] = list(payload)
+
+    def __str__(self: Message) -> str:
+        """
+        Format a `Message` object as a string.
+        """
+        tokens: List[str] = [
+            str(self.message_type),
+            self.timestamp.strftime(TIMESTAMP_FORMAT),
+            *self.payload
+        ]
+        return ",".join(tokens)
+
+    def __bytes__(self: Message) -> bytes:
+        """
+        Convert a `Message` object as a binary string`.
+        """
+        return str(self).encode("utf-8")
